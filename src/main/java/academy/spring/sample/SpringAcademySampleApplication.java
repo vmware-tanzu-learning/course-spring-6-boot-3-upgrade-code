1a4f15b0af1ce03dee3771c8bfe0fa5a1569d9a6
Robert McNees <rmcnees@vmware.com>
2023-05-18 11:10:05 -0400

    <boot-3-1-start> Initial state of Spring Boot 3.1 Lab

--- src/main/java/academy/spring/sample/SpringAcademySampleApplication.java
+++ src/main/java/academy/spring/sample/SpringAcademySampleApplication.java
@@ -2,6 +2,12 @@ package academy.spring.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class SpringAcademySampleApplication {
@@ -11,3 +17,26 @@ public class SpringAcademySampleApplication {
 	}
 
 }
+
+
+@Controller
+@ResponseBody
+class CustomerHttpController {
+	private final CustomerRepository customerRepository;
+
+	CustomerHttpController(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+
+	@GetMapping("/customers")
+	Iterable<Customer> customers() {
+		return this.customerRepository.findAll();
+	}
+
+}
+interface CustomerRepository extends CrudRepository<Customer, Integer> {
+
+	Iterable<Customer> findByName(String name);
+}
+
+record Customer (@Id Integer id, String name) {}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+delete from customer;
+insert into customer (name) values ('Phil');
+insert into customer (name) values ('Andy');
+insert into customer (name) values ('Madhura');
+insert into customer (name) values ('Olga');
+insert into customer (name) values ('Violeta');
+insert into customer (name) values ('Yuxin');
+insert into customer (name) values ('Dr. Syer');
+insert into customer (name) values ('Stéphane');
+insert into customer (name) values ('Jürgen');
+insert into customer (name) values ('Josh');
